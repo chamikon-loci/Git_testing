3ace150218a1a002541b9d7185b2b271e62b1c8e
chamikon-loci <chamikon.ks@gmail.com>
2026-09-10 22:34:39 +0700

    แก้ไข sort.java

---
+++ sort.java
@@ -0,0 +1,7 @@
+public class sort {
+    public static void main(String[] args){
+        System.out.println("i love somoxyz");
+        System.out.println("I hate this society");
+        System.out.println("สวัสดีชาวโลก");
+    }
+}
